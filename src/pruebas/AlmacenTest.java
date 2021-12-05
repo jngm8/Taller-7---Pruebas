aea6d2b281f8de2d7120225c9d36d7edb3f2bb8b
jngm8 <jn.gomezm1@uniandes.edu.co>
2021-12-04 23:19:31 -0500

    entrega

--- src/pruebas/AlmacenTest.java
+++ src/pruebas/AlmacenTest.java
@@ -1,9 +1,14 @@
 package pruebas;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import uniandes.cupi2.almacen.mundo.Almacen;
 import uniandes.cupi2.almacen.mundo.AlmacenException;
@@ -14,15 +19,21 @@ public class AlmacenTest
 {
 	private Almacen almacen;
 	
-	private Categoria categoria;
-	
-	@Before
-	public void setup ()
+	@BeforeEach
+	public void setup (File pArchivo)  throws AlmacenException
 	{
+	    {
+	        cargar( pArchivo );
+	    }
+	}
+	
+    @BeforeEach
+    public void cargar( File pArchivo ) throws AlmacenException
+    {
         try
         {
             BufferedReader in = new BufferedReader( new FileReader( pArchivo ) );
-            almacen = new Categoria( in.readLine( ), in );
+            Categoria almacen = new Categoria( in.readLine( ), in );
         }
         catch( Exception e )
         {
@@ -30,14 +41,72 @@ public class AlmacenTest
             throw new AlmacenException( "Error al leer el archivo.\n" + e.getMessage( ) );
 
         }
-	}
+    }
+	
 	
 	@Test
 	
-	public void test()
+	public void testConstructor()
 	
 	{
-		
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+
 	}
+	
+	@Test
+	public void testEliminarNodo()
+	
+	{
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+
+	}
+	
+	@Test
+	public void testAgregarNodo()
+	
+	{
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+
+	}
+	
+	@Test
+	public void testVenderProducto()
+	
+	{
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+
+	}
+	
+	
+	@Test
+	public void testBuscarNodo()
+	
+	{
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+
+	}
+	
+	@Test
+	public void testAgregarProducto()
+	
+	{
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+
+	}
+	
+	@Test
+	public void testEliminarProducto()
+	
+	{
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+		assertEquals("datos.txt",almacen.darCategoriaRaiz(),"Esta malo el archivo");
+
+	}
+	
 	
 }
--- src/pruebas/CategoriaTest.java
+++ src/pruebas/CategoriaTest.java
@@ -1,5 +1,37 @@
 package pruebas;
 
-public class CategoriaTest {
+import java.io.BufferedReader;
+import java.util.ArrayList;
 
+import uniandes.cupi2.almacen.mundo.AlmacenException;
+import uniandes.cupi2.almacen.mundo.Categoria;
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class CategoriaTest 
+
+{
+	private Categoria categoria;
+	
+	
+	@BeforeEach
+    public void Categoria( String pLinea, BufferedReader pLector ) throws AlmacenException
+    {
+        super( TIPO, pLinea.split( ";;;" )[ 1 ], pLinea.split( ";;;" )[ 2 ] );
+        nodosHijos = new ArrayList<>( );
+        try
+        {
+            String datos[] = pLinea.split( ";;;" );
+            int numHijos = Integer.parseInt( datos[ 3 ] );
+            while( numHijos-- > 0 )
+            {
+                agregarNodo( identificador, crearNodo( pLector ) );
+            }
+        }
+        catch( Exception e )
+        {
+            throw new AlmacenException( "Error al leer la marca:\n" + e.getMessage( ) );
+        }
+
+    }
 }
